940cc0d7b2e7336208dbdb7176a5716c6306e02f
tien192 <tranxuantien192@gmail.com>
2022-12-27 23:56:00 +0700

    day3

--- src/index.js
+++ src/index.js
@@ -1,34 +1,48 @@
-const express = require('express')
-const morgan = require('morgan')
-const { engine } = require('express-handlebars')
-const path = require('path')
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const path = require('path');
 
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+  extended:true
+}));
+app.use(express.json());
+
 
 //HTTP logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 //templates engine
 app.engine('hbs', engine({
   extname : '.hbs'
 }));
 app.set('view engine','hbs');
-app.set('views',path.join(__dirname, 'resources\\views'))
+app.set('views',path.join(__dirname, 'resources\\views'));
 
 
-app.get('/trang-chu', (req, res) => {
-  res.render('home')
-})
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
 app.get('/tin-tuc', (req, res) => {
-  res.render('news')
-})
+  res.render('news');
+});
 
+app.get('/search', (req, res) => {
+  res.render('search');
+});
+
+app.post('/search', (req, res) => {
+  console.log(req.body);
+  res.send('');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}/trang-chu`)
-})
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
