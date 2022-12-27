c780186d36a6b781a022fe7b7b9ade57a0cb04cd
tien192 <tranxuantien192@gmail.com>
2022-12-27 22:26:07 +0700

    day 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 const express = require('express')
+const morgan = require('morgan')
+const { engine } = require('express-handlebars')
+const path = require('path')
+
+
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+//HTTP logger
+app.use(morgan('combined'))
+
+//templates engine
+app.engine('hbs', engine({
+  extname : '.hbs'
+}));
+app.set('view engine','hbs');
+app.set('views',path.join(__dirname, 'resources\\views'))
+
+
 app.get('/trang-chu', (req, res) => {
-  res.send('Hello World!')
+  res.render('home')
 })
+
+app.get('/tin-tuc', (req, res) => {
+  res.render('news')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}/trang-chu`)
